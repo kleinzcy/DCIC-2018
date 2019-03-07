a5f13f62dcda0e9aed7686a48c46c9a045d316f3
kleinzcy <m15072762954@163.com>
2019-03-08 00:02:28 +0800

    no improve

--- model_zoo.py
+++ model_zoo.py
@@ -10,9 +10,12 @@ import lightgbm as lgb
 from sklearn.model_selection import KFold
 import xgboost as xgb
 
+from bayes_opt import BayesianOptimization
+
 import numpy as np
 import pandas as pd
 
+import time
 
 class my_lgb:
     def __init__(self, folds=5, seed=0):
@@ -22,7 +25,7 @@ class my_lgb:
         self.seed = seed
         self.oof = None
 
-    def inference_folds(self, X_train, y_train, X_test, param):
+    def inference_folds(self, X_train, y_train, X_test, param, cv=False):
         # 五折交叉验证
         folds = KFold(n_splits=self.folds, shuffle=True, random_state=self.seed)
         oof = np.zeros(X_train.shape[0])
@@ -42,21 +45,25 @@ class my_lgb:
                             early_stopping_rounds=200)
             oof[val_idx] = np.rint(clf.predict(X_train[val_idx], num_iteration=clf.best_iteration))
 
-            predictions += clf.predict(X_test, num_iteration=clf.best_iteration) / folds.n_splits
+            if not cv:
+                predictions += clf.predict(X_test, num_iteration=clf.best_iteration) / folds.n_splits
 
             self.feature_importance.append(clf.feature_importance())
 
-        self.results = np.rint(predictions).astype('int64')
+        if not cv:
+            self.results = np.rint(predictions).astype('int64')
 
         self.oof = oof
 
-
         print("score: {:.8f}, MAE: {}".format(self.score(oof, y_train), self.MAE(oof, y_train)))
+
+        return self.score(self.oof, y_train)
 
     def score(self, pre, truth):
         return 1 / (self.MAE(pre, truth) + 1)
 
-    def MAE(self, pre, truth):
+    @staticmethod
+    def MAE(pre, truth):
         return abs((pre.astype(int) - truth)).mean()
 
     def submit(self, example_dir='dataset/submit_example.csv',output_name = 'default'):
@@ -65,8 +72,54 @@ class my_lgb:
         sub_df.to_csv("output/{}.csv".format(output_name), index=False)
 
     def importance_feature(self, feature_name):
-        _importance_feture = np.array(self.feature_importance).mean(axis=0).astype(int)
-        return pd.DataFrame({'score':_importance_feture}, index=feature_name).sort_values(by='score', ascending=False)
+        _importance_feature = np.array(self.feature_importance).mean(axis=0).astype(int)
+        return pd.DataFrame({'score':_importance_feature}, index=feature_name).sort_values(by='score', ascending=False)
+
+    def optimize_lgb(self, X_train, y_train, X_test, general_params):
+        # 'min_data_in_leaf': (10, 50),
+        # 'bagging_fraction': (0.3, 1.0),
+        #  'feature_fraction': (0.3, 1.0),
+        params ={
+            'num_leaves': (30, 40),
+            'lambda_l1': (1, 10),
+            'lambda_l2': (0, 5)
+        }
+
+        def lgb_cv(num_leaves, lambda_l1, lambda_l2):
+            param = {
+                # general parameters
+                'objective': general_params['objective'],
+                'boosting': general_params['boosting'],
+                'metric': general_params['metric'],
+                'bagging_freq': general_params['bagging_freq'],
+                'learning_rate': general_params['learning_rate'],
+                'verbosity': general_params["verbosity"],
+                'max_depth': general_params['max_depth'],
+                # tuning parameters
+                'num_leaves': int(num_leaves),
+                'bagging_fraction': 0.5,
+                'feature_fraction': 0.5,
+                'lambda_l1': lambda_l1,
+                'lambda_l2': lambda_l2
+            }
+
+            return self.inference_folds(X_train, y_train, X_test, param)
+
+        lgbBO = BayesianOptimization(lgb_cv, params)
+
+        start_time = time.time()
+        lgbBO.maximize(init_points=5, n_iter=10)
+        end_time = time.time()
+        print("Final result:{}, spend {}s".format(lgbBO.max, start_time-end_time))
+        best_params = lgbBO.res['max']
+        best_params['objective'] = general_params['objective']
+        best_params['boosting'] = general_params['boosting']
+        best_params['metric'] = general_params['metric']
+        best_params['objective'] = general_params['objective']
+        best_params['bagging_freq'] = general_params['learning_rate']
+
+        return best_params
+
 
 class my_xgb:
     def __init__(self, folds=5):
